85cc8046224381535b56b76e1682b8bc7abda6e4
stovet <steven.topalian@gmail.com>
2022-01-11 11:28:19 -0500

    erased commented out code

--- src/components/UpDownCounter.jsx
+++ src/components/UpDownCounter.jsx
@@ -3,9 +3,6 @@ import './UpDownCounter.css';
 
 function UpDownCounter(){
     const [count, setCount] = useState(0);
-    // if(setCount(1){
-    //     let counter = count + 1;
-    // } else  counter = count -1;
     return(
         <div className="UpDownCounter">
                 <button onClick={() => setCount(count + 1)}>Up</button>
